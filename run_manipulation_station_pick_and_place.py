161e7ff6f81478c0c76f6d1a3ac61264e94bf14e
Greg Izatt <gizatt@mit.edu>
2019-06-10 20:24:01 -0400

    Task space planning!

--- run_manipulation_station_pick_and_place.py
+++ run_manipulation_station_pick_and_place.py
@@ -24,7 +24,8 @@ from pydrake.systems.framework import (BasicVector, DiagramBuilder,
                                        LeafSystem)
 from pydrake.systems.meshcat_visualizer import MeshcatVisualizer
 from pydrake.systems.primitives import FirstOrderLowPassFilter
-from pydrake.trajectories import PiecewisePolynomial
+from pydrake.trajectories import (
+    PiecewisePolynomial, PiecewiseQuaternionSlerp)
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
@@ -40,7 +41,7 @@ def main():
 
     # Set up the PlanRunner
     plan_runner = builder.AddSystem(
-        RobotPlanRunner(is_discrete=False, control_period_sec=1e-3))
+        RobotPlanRunner(is_discrete=True, control_period_sec=1e-3))
     builder.Connect(station.GetOutputPort("iiwa_position_measured"),
                     plan_runner.GetInputPort("iiwa_position_measured"))
     builder.Connect(station.GetOutputPort("iiwa_velocity_estimated"),
@@ -54,16 +55,41 @@ def main():
 
     # Set up a simple PlanSender that sends multiple
     # plans in sequence
-    t_knots = np.array([0., 1., 2.])
-    q_knots = np.array([
-        [0, 0, 0, 0, 0, 0, 0],
-        [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
-        [1, 1, 1, 1, 1, 1, 1]
-    ]).T
-    traj = PiecewisePolynomial.Cubic(
-        t_knots, q_knots, np.zeros(7), np.zeros((7)))
-    plan_1 = PlanData(PlanType.kJointSpacePlan, traj)
-    plan_sender = builder.AddSystem(PlanSender([plan_1]))
+    def MakeQPlanData():
+        t_knots = np.array([0., 2., 4.])
+        q_knots = np.array([
+            [0., 0.6, 0., -1.75, 0., 1., 0.],
+            [0.1, 0.6, 0., -1.75, 0., 1., 0.],
+            [-0.1, 0.6, 0., -1.75, 0., 1., 0.]]).T
+        q_traj = PiecewisePolynomial.Cubic(
+            t_knots, q_knots, np.zeros(7), np.zeros((7)))
+        return PlanData(PlanType.kJointSpacePlan,
+                          joint_traj=q_traj)
+    def MakeEEPlanData():
+        t_knots = np.array([0., 2., 4.])
+        ee_xyz_knots = np.array([
+            [0.7, 0., 0.1],
+            [0.7, 0.2, 0.3],
+            [0.7, -0.2, 0.1]]).T
+        ee_quat_knots = [
+            RollPitchYaw(0., np.pi, 0).ToQuaternion(),
+            RollPitchYaw(0., np.pi, 0).ToQuaternion(),
+            RollPitchYaw(0., np.pi, 0).ToQuaternion()
+            ]
+        ee_xyz_traj = PiecewisePolynomial.FirstOrderHold(
+            t_knots, ee_xyz_knots)
+        ee_quat_traj = PiecewiseQuaternionSlerp(
+            t_knots, ee_quat_knots)
+        return PlanData(PlanType.kTaskSpacePlan,
+                        ee_data=PlanData.EeData(
+                            p_ToQ_T=np.zeros(3),
+                            ee_xyz_traj=ee_xyz_traj,
+                            ee_quat_traj=ee_quat_traj))
+
+    q_plan = MakeQPlanData()
+    ee_plan = MakeEEPlanData()
+
+    plan_sender = builder.AddSystem(PlanSender([q_plan, ee_plan]))
     builder.Connect(plan_sender.GetOutputPort("plan_data"),
                     plan_runner.GetInputPort("plan_data"))
     builder.Connect(station.GetOutputPort("iiwa_position_measured"),
@@ -87,7 +113,7 @@ def main():
     simulator = Simulator(diagram, diagram_context)
     simulator.set_publish_every_time_step(False)
     simulator.set_target_realtime_rate(1.0)
-    simulator.AdvanceTo(10.0)
+    simulator.AdvanceTo(plan_sender.get_all_plans_duration())
 
 
 if __name__ == "__main__":

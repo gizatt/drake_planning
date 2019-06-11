482299ebcb7bd9c2d396848577bf1b4299f79808
Greg Izatt <gizatt@mit.edu>
2019-06-10 21:57:22 -0400

    Hack in a camera grabber / visualizer

--- run_manipulation_station_pick_and_place.py
+++ run_manipulation_station_pick_and_place.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import sys
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -20,12 +21,65 @@ from pydrake.manipulation.robot_plan_runner import (
 )
 from pydrake.math import RigidTransform, RollPitchYaw
 from pydrake.systems.analysis import Simulator
-from pydrake.systems.framework import (BasicVector, DiagramBuilder,
-                                       LeafSystem)
+from pydrake.systems.framework import (
+    AbstractValue, BasicVector, DiagramBuilder, LeafSystem)
 from pydrake.systems.meshcat_visualizer import MeshcatVisualizer
 from pydrake.systems.primitives import FirstOrderLowPassFilter
+from pydrake.systems.sensors import (
+    Image,
+    PixelFormat,
+    PixelType
+)
 from pydrake.trajectories import (
-    PiecewisePolynomial, PiecewiseQuaternionSlerp)
+    PiecewisePolynomial,
+    PiecewiseQuaternionSlerp
+)
+
+
+class CameraCaptureSystem(LeafSystem):
+    ''' Example system that periodically
+    grabs RGB-D image inputs. If given matplotlib axes,
+    draws the RGB and Depth images to them when they're grabbed. '''
+    def __init__(self, grab_period=0.1, ax_rgb=None, ax_depth=None):
+        LeafSystem.__init__(self)
+
+        self.set_name('camera_capture_system')
+        self.DeclarePeriodicPublish(grab_period, 0.0)
+        # RGB and D image input ports.
+        # Declaring input ports requires supplying a prototype
+        # of the input type.
+        prototype_rgb_image = Image[PixelType.kRgba8U](0, 0)
+        prototype_depth_image = Image[PixelType.kDepth16U](0, 0)
+        self.DeclareAbstractInputPort("rgb_image",
+                                      AbstractValue.Make(
+                                        prototype_rgb_image))
+        self.DeclareAbstractInputPort("depth_image",
+                                      AbstractValue.Make(
+                                        prototype_depth_image))
+
+        self.rgb_data = None
+        self.depth_data = None
+        if ax_rgb is not None:
+            self.rgb_data = ax_rgb.imshow(np.zeros([1, 1, 4]))
+        if ax_depth is not None:
+            self.depth_data = ax_depth.imshow(np.zeros([1, 1]))
+
+    def DoPublish(self, context, event):
+        # TODO(russt): Change this to declare a periodic event with a
+        # callback instead of overriding DoPublish, pending #9992.
+        LeafSystem.DoPublish(self, context, event)
+        print("Curr sim time: ", context.get_time())
+
+        rgb_image = self.EvalAbstractInput(context, 0).get_value()
+        depth_image = self.EvalAbstractInput(context, 0).get_value()
+
+        if self.rgb_data is not None:
+            self.rgb_data.set_data(rgb_image.data)
+            plt.gcf().canvas.draw()
+        if self.depth_data is not None:
+            self.depth_data.set_data(depth_image.data.squeeze())
+            plt.gcf().canvas.draw()
+
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
@@ -100,6 +154,25 @@ def main():
         station.get_scene_graph(), zmq_url=args.meshcat))
     builder.Connect(station.GetOutputPort("pose_bundle"),
                     meshcat.get_input_port(0))
+
+    # Hook up cameras
+    fig = plt.figure()
+    fig.show()
+    camera_capture_systems = []
+    camera_names = station.get_camera_names()
+    for cam_i, name in enumerate(camera_names):
+        ax_rgb = plt.subplot(len(camera_names), 2, cam_i*2 + 1)
+        ax_depth = plt.subplot(len(camera_names), 2, cam_i*2 + 2)
+        camera_capture_system = builder.AddSystem(CameraCaptureSystem(
+            grab_period=1.0,
+            ax_rgb=ax_rgb,
+            ax_depth=ax_depth))
+        camera_capture_system.set_name("capture_%d" % cam_i)
+        builder.Connect(station.GetOutputPort("camera_%s_rgb_image" % name),
+                        camera_capture_system.GetInputPort("rgb_image"))
+        builder.Connect(station.GetOutputPort("camera_%s_depth_image" % name),
+                        camera_capture_system.GetInputPort("depth_image"))
+        camera_capture_systems.append(camera_capture_system)
 
     # Remaining input ports need to be tied up.
     diagram = builder.Build()

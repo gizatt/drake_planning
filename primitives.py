af50397412d9409208c45080feadecd7cb7bc651
Greg Izatt <gizatt@mit.edu>
2019-06-12 23:52:07 -0400

    Cleanup

---
+++ primitives.py
@@ -0,0 +1,87 @@
+import os
+import sys
+
+import numpy as np
+
+from pydrake.math import RigidTransform, RollPitchYaw
+from pydrake.trajectories import PiecewisePolynomial
+
+
+# TODO(gizatt) Primitives might want to subclass subclass,
+# since whether they're valid is a symbol itself.
+class IiwaAndGripperPrimitive(object):
+    ''' Interface for primitive actions that can be run
+    on the ManipulationStation (Kuka IIWA + Schunk gripper).
+    Makes all decisions based on an MBP context.'''
+    def __init__(self, name, mbp):
+        self.name = name
+        self.mbp = mbp
+
+    def is_valid(self, mbp_context):
+        raise NotImplementedError()
+
+    def generate_rpyxyz_and_gripper_trajectory(self, mbp_context):
+        raise NotImplementedError()
+
+
+class MoveBoxPrimitive(IiwaAndGripperPrimitive):
+    ''' Interface for primitive actions that can be run
+    on the ManipulationStation (Kuka IIWA + Schunk gripper).
+    Makes all decisions based on an MBP context.'''
+    def __init__(self, name, mbp, body_name, goal_position):
+        IiwaAndGripperPrimitive.__init__(self, name, mbp)
+        self.goal_position = np.array(goal_position)
+        self.goal_body = mbp.GetBodyByName(body_name)
+
+    def is_valid(self, mbp_context):
+        # TODO(gizatt) This is probably not always true
+        return True
+
+    def generate_rpyxyz_and_gripper_trajectory(self, mbp_context):
+        start_position = self.mbp.EvalBodyPoseInWorld(mbp_context, self.goal_body).translation()
+        start_ee_pose = self.mbp.EvalBodyPoseInWorld(
+            mbp_context, self.mbp.GetBodyByName("iiwa_link_7"))
+        grasp_offset = np.array([0.0, 0., 0.2]) # Amount *all* target points are shifted up
+        up_offset = np.array([0., 0., 0.1])      # Additional amount to lift objects off of table
+        # Timing:
+        #    0       :  t_reach: move over object
+        #    t_reach :  t_touch: move down to object
+        #    t_touch :  t_grasp: close gripper
+        #    t_grasp :  t_lift : pick up object
+        #    t_lift  :  t_move : move object over destination
+        #    t_move  :  t_down : move object down
+        #    t_down  :  t_drop : open gripper
+        #    t_drop  :  t_done : rise back up
+        t_each = 1.0
+        t_reach = 0. + t_each 
+        t_touch = t_reach + t_each 
+        t_grasp = t_touch + t_each 
+        t_lift = t_grasp + t_each 
+        t_move = t_lift + t_each 
+        t_down = t_move + t_each 
+        t_drop = t_down + t_each 
+        t_done = t_drop + t_each 
+        ts = np.array([0., t_reach, t_touch, t_grasp, t_lift, t_move, t_down, t_drop, t_done])
+        ee_xyz_knots = np.vstack([
+            start_ee_pose.translation(),
+            start_position + up_offset,
+            start_position,
+            start_position,
+            start_position + up_offset,
+            self.goal_position + up_offset,
+            self.goal_position,
+            self.goal_position,
+            self.goal_position + up_offset,
+        ]).T
+        ee_xyz_knots += np.tile(grasp_offset, [len(ts), 1]).T
+
+        ee_rpy_knots = np.array([-np.pi, 0., np.pi/2.])
+        ee_rpy_knots = np.tile(ee_rpy_knots, [len(ts), 1]).T
+        ee_rpyxyz_knots = np.vstack([ee_rpy_knots, ee_xyz_knots])
+        ee_rpyxyz_traj = PiecewisePolynomial.FirstOrderHold(
+            ts, ee_rpyxyz_knots)
+
+        gripper_knots = np.array([[0.1, 0.1, 0.1, 0., 0., 0., 0., 0.1, 0.1]])
+        gripper_traj = PiecewisePolynomial.FirstOrderHold(
+            ts, gripper_knots)
+        return ee_rpyxyz_traj, gripper_traj
--- run_manipulation_station_pick_and_place.py
+++ run_manipulation_station_pick_and_place.py
@@ -42,101 +42,13 @@ from pydrake.systems.primitives import (
     ConstantVectorSource,
     TrajectorySource
 )
-from pydrake.systems.sensors import (
-    Image,
-    PixelFormat,
-    PixelType
-)
-from pydrake.trajectories import (
-    PiecewisePolynomial,
-    PiecewiseQuaternionSlerp
-)
 
 from underactuated.planar_scenegraph_visualizer import PlanarSceneGraphVisualizer
 from mouse_keyboard_teleop import MouseKeyboardTeleop, print_instructions
 from differential_ik import DifferentialIK
 
 from symbol_map import *
-
-
-# TODO(gizatt) Primitives might want to subclass subclass,
-# since whether they're valid is a symbol itself.
-class IiwaAndGripperPrimitive(object):
-    ''' Interface for primitive actions that can be run
-    on the ManipulationStation (Kuka IIWA + Schunk gripper).
-    Makes all decisions based on an MBP context.'''
-    def __init__(self, name, mbp):
-        self.name = name
-        self.mbp = mbp
-
-    def is_valid(self, mbp_context):
-        raise NotImplementedError()
-
-    def generate_rpyxyz_and_gripper_trajectory(self, mbp_context):
-        raise NotImplementedError()
-
-
-class MoveBoxPrimitive(IiwaAndGripperPrimitive):
-    ''' Interface for primitive actions that can be run
-    on the ManipulationStation (Kuka IIWA + Schunk gripper).
-    Makes all decisions based on an MBP context.'''
-    def __init__(self, name, mbp, body_name, goal_position):
-        IiwaAndGripperPrimitive.__init__(self, name, mbp)
-        self.goal_position = np.array(goal_position)
-        self.goal_body = mbp.GetBodyByName(body_name)
-
-    def is_valid(self, mbp_context):
-        # TODO(gizatt) This is probably not always true
-        return True
-
-    def generate_rpyxyz_and_gripper_trajectory(self, mbp_context):
-        start_position = self.mbp.EvalBodyPoseInWorld(mbp_context, self.goal_body).translation()
-        start_ee_pose = self.mbp.EvalBodyPoseInWorld(
-            mbp_context, self.mbp.GetBodyByName("iiwa_link_7"))
-        grasp_offset = np.array([0.0, 0., 0.2]) # Amount *all* target points are shifted up
-        up_offset = np.array([0., 0., 0.1])      # Additional amount to lift objects off of table
-        # Timing:
-        #    0       :  t_reach: move over object
-        #    t_reach :  t_touch: move down to object
-        #    t_touch :  t_grasp: close gripper
-        #    t_grasp :  t_lift : pick up object
-        #    t_lift  :  t_move : move object over destination
-        #    t_move  :  t_down : move object down
-        #    t_down  :  t_drop : open gripper
-        #    t_drop  :  t_done : rise back up
-        t_each = 1.0
-        t_reach = 0. + t_each 
-        t_touch = t_reach + t_each 
-        t_grasp = t_touch + t_each 
-        t_lift = t_grasp + t_each 
-        t_move = t_lift + t_each 
-        t_down = t_move + t_each 
-        t_drop = t_down + t_each 
-        t_done = t_drop + t_each 
-        ts = np.array([0., t_reach, t_touch, t_grasp, t_lift, t_move, t_down, t_drop, t_done])
-        ee_xyz_knots = np.vstack([
-            start_ee_pose.translation(),
-            start_position + up_offset,
-            start_position,
-            start_position,
-            start_position + up_offset,
-            self.goal_position + up_offset,
-            self.goal_position,
-            self.goal_position,
-            self.goal_position + up_offset,
-        ]).T
-        ee_xyz_knots += np.tile(grasp_offset, [len(ts), 1]).T
-
-        ee_rpy_knots = np.array([-np.pi, 0., -np.pi/2.])
-        ee_rpy_knots = np.tile(ee_rpy_knots, [len(ts), 1]).T
-        ee_rpyxyz_knots = np.vstack([ee_rpy_knots, ee_xyz_knots])
-        ee_rpyxyz_traj = PiecewisePolynomial.FirstOrderHold(
-            ts, ee_rpyxyz_knots)
-
-        gripper_knots = np.array([[0.1, 0.1, 0.1, 0., 0., 0., 0., 0.1, 0.1]])
-        gripper_traj = PiecewisePolynomial.FirstOrderHold(
-            ts, gripper_knots)
-        return ee_rpyxyz_traj, gripper_traj
+from primitives import *
 
 
 class TaskExectionSystem(LeafSystem):
@@ -234,8 +146,6 @@ class SymbolLoggerSystem(LeafSystem):
         self.DeclarePeriodicPublish(grab_period, 0.0)
 
         # Take robot state vector as input.
-        prototype_rgb_image = Image[PixelType.kRgba8U](0, 0)
-        prototype_depth_image = Image[PixelType.kDepth16U](0, 0)
         self.DeclareVectorInputPort("mbp_state_vector",
                                     BasicVector(mbp.num_positions() +
                                                 mbp.num_velocities()))
@@ -389,7 +299,7 @@ def main():
                         station.GetInputPort("iiwa_position"))
 
 
-        goal_position = [0.5, 0., 0.025]
+        goal_position = [0.5, 0., 0.05]
         goal_delta = 0.05
         symbol_list = [
             SymbolL2Close("blue_box_in_goal", "blue_box", goal_position, goal_delta),
